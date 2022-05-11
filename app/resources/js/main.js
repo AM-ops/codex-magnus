4a742aa756856a5ebf561084d7ea3b2da3f57171
AM-ops <affaanm94@yahoo.com>
2022-05-11 22:31:59 +0200

    Added first slide
    First slide completed including formatting

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -3,22 +3,6 @@
 // Feel free to use any frontend framework you like :)
 // See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
 
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
-
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
-
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
-}
-
 function setTray() {
     if(NL_MODE != "window") {
         console.log("INFO: Tray menu is only available in the window mode.");
@@ -35,29 +19,24 @@ function setTray() {
     Neutralino.os.setTray(tray);
 }
 
-function onTrayMenuItemClicked(event) {
-    switch(event.detail.id) {
-        case "VERSION":
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
-    }
-}
-
 function onWindowClose() {
     Neutralino.app.exit();
 }
 
+Neutralino.window.setTitle('Principles of Good Design');
+Neutralino.window.setAlwaysOnTop(true);
+
+Neutralino.window.setSize({
+    width: 1000,
+    height: 500,
+
+    resizable: false
+});
+
 Neutralino.init();
 
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
 Neutralino.events.on("windowClose", onWindowClose);
 
 if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
     setTray();
 }
-
-showInfo();
